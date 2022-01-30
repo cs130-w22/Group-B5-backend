154492677b12e3d1d24c1a860e93954a32f70337
Thomas Kamm <tgkamm@yahoo.com>
2022-01-30 15:54:40 -0800

    add basic error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const loginRoute = require('./routes/login');
-const signupRoute = require('./routes/signup');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // routers
-app.use('/login', loginRoute);
-app.use('/signup', signupRoute);
+app.use('/auth', authRoutes);
+
+app.get("*", (res, next) => {
+	res.status(404).send("Page not found")
+})
+	
+app.use((error, req, res, next) => {
+	return res.status(500).json({
+		error: {
+			message: error.message || "Something went wrong."
+		}
+	})
+})
 
 // start server on port
 const PORT = 8080

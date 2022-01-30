0e102e631dede35b524f88178dc7155224621234
Thomas Kamm <tgkamm@yahoo.com>
2022-01-30 15:22:06 -0800

    Add support for password encryption

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const models = require('./models/index');
+const bcrypt = require('bcryptjs')
 
 // connect to mongoDB database
-mongoose.connect('mongodb://127.0.0.1:27017/leetracerDB');
+mongoose.connect('mongodb://127.0.0.1:27017/leetracerDB').catch((e) => { console.log("Failed to connect to the db: \n", e); process.exit(); })
 
 // insert a new username/password combination into the database
 // return true on success, false on failure (if username already exists)
 async function addNewUser(name, password) {
-	const newUser = new models.User({ name: name, password: password });
 	try {
+		const hash = await bcrypt.hash(password, 8);
+		const newUser = new models.User({ name: name, password: hash });
 		await newUser.save();
 	}
 	catch (error) {
@@ -20,7 +22,12 @@ async function addNewUser(name, password) {
 // returns true if name/password exist as a user in the database, false otherwise
 async function checkPassword(name, password) {
 	try {
-		await models.User.findOne({ name: name, password: password }).orFail();
+		let user = await models.User.findOne({ name: name });
+		if(!user) throw new Error(`User could not be found with username: ${name}`);
+
+		let check = await bcrypt.compare(password, user.password);
+		if(!check) throw new Error(`Incorrect password for username ${name}`)
+
 	}
 	catch (error) {
 		return false;

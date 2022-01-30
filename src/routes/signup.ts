15eb511dcf0df2ddf4442fb71c0f912f15bbe0ff
grifkin <gerifkin@gmail.com>
2022-01-30 14:01:11 -0800

    Removed password confirmation check from signup route

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -5,17 +5,11 @@ import * as db from '../db';
 router.post('/', async function(req, res, next) {
 	let username = req.body.username;
 	let password = req.body.password;
-	let confirmPassword = req.body.confirmPassword;
 
 	// check for missing fields
-	if(username == undefined || password == undefined || confirmPassword == undefined ||
-	   username.length == 0 || password.length == 0 || confirmPassword.length == 0) {
+	if(username == undefined || password == undefined ||
+	   username.length == 0 || password.length == 0) {
 		return res.status(400).json({ err: true, message: "One or more fields are missing or incorrect" });
-	}
-
-	// check if password and confirmPassword are the same
-	if(password != confirmPassword) {
-		return res.status(401).json({ err: true, message: "Password does not match confirm password" });
 	}
 
 	// try adding new user

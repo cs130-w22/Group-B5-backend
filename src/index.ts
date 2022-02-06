7604ac42b326ee6105fc649ce186d9b77a4d392b
Thomas Kamm <tgkamm@yahoo.com>
2022-02-05 16:48:37 -0800

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require("cors")
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const leetcodeRoutes = require('./routes/leetcode');
 
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 

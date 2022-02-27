2e60c579eb24b59edeef025ec6467372f2d47159
Thomas Kamm <tgkamm@yahoo.com>
2022-02-26 19:49:19 -0800

    accept port environment variable

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ app.use((error, req, res, next) => {
 })
 
 // start server on port
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
 	console.log('Server started on port ' + PORT);
 });

1f2654278b7a9ac7211470b3da00051993341e78
lfu7 <lawrencefu@g.ucla.edu>
2022-03-05 22:49:49 -0800

    finished submission tests

--- test/submission.ts
+++ test/submission.ts
@@ -1,38 +1,60 @@
-import Leetcode from "../src/lib/leetcode";
-import Problem from '../src/lib/problem';
-import { Credit, EndPoint, SubmissionStatus } from '../src/utils/interfaces';
-import Submission from "../src/lib/submission";
-
 import { expect } from 'chai';
+import Dotenv from 'dotenv';
 import 'mocha';
+import Leetcode from '../src/lib/leetcode';
+import Problem from '../src/lib/problem';
+import Submission from '../src/lib/submission';
+import { EndPoint, ProblemStatus, SubmissionStatus } from '../src/utils/interfaces';
 
 const path = require('path');
 const fs = require('fs')
 
+describe("# Submission", async function () {
+    this.enableTimeouts(false);
+    before(async () => {
+        Dotenv.config();
+        const credit = {
+            session: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiMzcwMTA5MyIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9oYXNoIjoiMmY5ZDIyNWUwYWMyYWU2OGJkMzYwZGU2NDllNmI4NzczN2FjMGJkNiIsImlkIjozNzAxMDkzLCJlbWFpbCI6Imxhd3JlbmNlZnVAdWNsYS5lZHUiLCJ1c2VybmFtZSI6ImxmdTciLCJ1c2VyX3NsdWciOiJsZnU3IiwiYXZhdGFyIjoiaHR0cHM6Ly9hc3NldHMubGVldGNvZGUuY29tL3VzZXJzL2xmdTcvYXZhdGFyXzE2MzE4MzUxMDIucG5nIiwicmVmcmVzaGVkX2F0IjoxNjQ2NDY0Mjc0LCJpcCI6IjI2MDM6ODAwMTo2OTAxOjI1ODc6ZTE4NTozNjI0OmE4Y2Y6MzJjMCIsImlkZW50aXR5IjoiNDZjYTYxNDQ1MjY2NzYzMTQzNDgxODUyNjg4MmYxMDAiLCJzZXNzaW9uX2lkIjoxODY1NDg0NywiX3Nlc3Npb25fZXhwaXJ5IjoxMjA5NjAwfQ.VkECypOZHGz4-yBdtFDU9V6rKoMzB0oB8vAlTqB_HVQ",
+            csrfToken: "cL6RT3ctYA2pxF1t9ECNcWVkEQyoQUNVbKihLai2DnIIjgKlmNJIiDbKk6KMgU5p",
+        };
+        const endpoint = EndPoint["US"];
+        const leetcode: Leetcode = await Leetcode.build2(
+            credit,
+            endpoint
+        );
+    })
 
-describe("# Leetcode Submission Tests", () => {
-
-    const session: string = process.env.LEETCODE_SESSION || "error";
-    const csrfToken: string = process.env.LEETCODE_CSRF || "error";
-    const credit = {
-        session: session,
-        csrfToken: csrfToken
-    };
-    const endpoint = EndPoint["US"];
-
-    describe("# Testing Accepted Solution", () => {
-        // accepted test case
+    describe("Testing Accepted Submission", async function () {
         let submission: Submission;
+        let s: string;
         before(async () => {
+
             fs.readFile(path.join(__dirname, "/test_cases/accepted.py"), async (err: any, data: any) => {
                 if (err) throw err;
-                const s = data.toString()
-                submission = await submit(credit, endpoint, "two-sum", "python3", s);
+                s = data.toString()
             });
-        
-        })
 
-        it('Submission Object must be instance of Submission', async function () {
+            const problem: Problem = new Problem("two-sum");
+            await problem.detail();
+
+            await new Promise(r => setTimeout(r, 10000));
+
+            submission = await problem.submit("python3", s);
+
+            for (let i = 0; i < 10; i++) {
+
+                // timeout two seconds
+                await new Promise(r => setTimeout(r, 2000));
+                await submission.detail()
+
+                // if the submission is ready, break the loop
+                if (submission.status !== SubmissionStatus["Submission Not Ready"]) {
+                    break;
+                }
+            }
+        });
+
+        it("Should be instance of Submission", () => {
             expect(submission).to.instanceOf(Submission);
         });
 
@@ -41,19 +63,37 @@ describe("# Leetcode Submission Tests", () => {
         });
     });
 
-    describe("# Testing Compile Error Solution", () => {
-        // accepted test case
+    describe("Testing Compile Error Submission", async function () {
         let submission: Submission;
+        let s: string;
         before(async () => {
+
             fs.readFile(path.join(__dirname, "/test_cases/compile_error.cpp"), async (err: any, data: any) => {
                 if (err) throw err;
-                const s = data.toString()
-                const submission: Submission = await submit(credit, endpoint, "two-sum", "cpp", s);
-            })
-        
-        })
+                s = data.toString()
+            });
 
-        it('Submission Object must be instance of Submission', async function () {
+            const problem: Problem = new Problem("two-sum");
+            await problem.detail();
+
+            await new Promise(r => setTimeout(r, 10000));
+
+            submission = await problem.submit("cpp", s);
+
+            for (let i = 0; i < 10; i++) {
+
+                // timeout two seconds
+                await new Promise(r => setTimeout(r, 2000));
+                await submission.detail()
+
+                // if the submission is ready, break the loop
+                if (submission.status !== SubmissionStatus["Submission Not Ready"]) {
+                    break;
+                }
+            }
+        });
+
+        it("Should be instance of Submission", () => {
             expect(submission).to.instanceOf(Submission);
         });
 
@@ -62,40 +102,76 @@ describe("# Leetcode Submission Tests", () => {
         });
     });
 
-    describe("# Testing Incorrect Solution", () => {
-        // accepted test case
+    describe("Testing Wrong Answer Submission", async function () {
         let submission: Submission;
+        let s: string;
         before(async () => {
-            fs.readFile(path.join(__dirname, "/test_cases/incorrect.py"), async (err: any, data: any) => {
-                if (err) throw err;
-                const s = data.toString()
-                const submission: Submission = await submit(credit, endpoint, "add-two-numbers", "python3", s);
-            })
-        
-        })
 
-        it('Submission Object must be instance of Submission', async function () {
+            fs.readFile(path.join(__dirname,"/test_cases/incorrect.py"), async (err: any, data: any) => {
+                if (err) throw err;
+                s = data.toString()
+            });
+
+            const problem: Problem = new Problem("add-two-numbers");
+            await problem.detail();
+
+            await new Promise(r => setTimeout(r, 10000));
+
+            submission = await problem.submit("python3", s);
+
+            for (let i = 0; i < 10; i++) {
+
+                // timeout two seconds
+                await new Promise(r => setTimeout(r, 2000));
+                await submission.detail()
+
+                // if the submission is ready, break the loop
+                if (submission.status !== SubmissionStatus["Submission Not Ready"]) {
+                    break;
+                }
+            }
+        });
+
+        it("Should be instance of Submission", () => {
             expect(submission).to.instanceOf(Submission);
         });
 
-        it('Submission status should be Incorrect', function () {
+        it('Submission status should be Wrong Answer', function () {
             expect(submission.status).to.equal(SubmissionStatus["Wrong Answer"]);
         });
     });
 
-    describe("# Testing Runtime Error Solution", () => {
-        // accepted test case
+    describe("Testing Runtime Error Submission", async function () {
         let submission: Submission;
+        let s: string;
         before(async () => {
-            fs.readFile(path.join(__dirname, "/test_cases/runtime_error.py"), async (err: any, data: any) => {
-                if (err) throw err;
-                const s = data.toString()
-                const submission: Submission = await submit(credit, endpoint, "median-of-two-sorted-arrays", "python3", s);
-            })
-        
-        })
 
-        it('Submission Object must be instance of Submission', async function () {
+            fs.readFile(path.join(__dirname,"/test_cases/runtime_error.py"), async (err: any, data: any) => {
+                if (err) throw err;
+                s = data.toString()
+            });
+
+            const problem: Problem = new Problem("median-of-two-sorted-arrays");
+            await problem.detail();
+
+            await new Promise(r => setTimeout(r, 10000));
+
+            submission = await problem.submit("python3", s);
+
+            for (let i = 0; i < 10; i++) {
+
+                // timeout two seconds
+                await new Promise(r => setTimeout(r, 2000));
+                await submission.detail()
+
+                // if the submission is ready, break the loop
+                if (submission.status !== SubmissionStatus["Submission Not Ready"]) {
+                    break;
+                }
+            }
+        });
+
+        it("Should be instance of Submission", () => {
             expect(submission).to.instanceOf(Submission);
         });
 
@@ -103,66 +179,4 @@ describe("# Leetcode Submission Tests", () => {
             expect(submission.status).to.equal(SubmissionStatus["Runtime Error"]);
         });
     });
-
-    describe("# Testing Time Limit Exceeded Solution", () => {
-        // accepted test case
-        let submission: Submission;
-        before(async () => {
-            fs.readFile(path.join(__dirname, "/test_cases/tle.py"), async (err: any, data: any) => {
-                if (err) throw err;
-                const s = data.toString()
-                const submission: Submission = await submit(credit, endpoint, "find-substring-with-given-hash-value", "python3", s);
-        
-                console.log(submission.status);
-                console.log(submission.input);
-                console.log(submission.expected_output);
-            })
-        })
-
-        it('Submission Object must be instance of Submission', async function () {
-            expect(submission).to.instanceOf(Submission);
-        });
-
-        it('Submission status should be Time Limit Exceeded', function () {
-            expect(submission.status).to.equal(SubmissionStatus["Time Limit Exceeded"]);
-        });
-    });
-
-    
-
 });
-
-
-
-
-async function submit(credit: Credit, endpoint: EndPoint, slug: string, lang: string, code: string): Promise<Submission> {
-
-    // configures global Helper module to take submit under a new user
-    let leetcode: Leetcode;
-
-    // created a new build method for leetcode object, since old one does not work correctly
-    leetcode = await Leetcode.build2(credit, endpoint);
-
-    // creates a new problem based on the slug
-    const problem: Problem = new Problem(slug);
-    await problem.detail();
-    
-    const submission: Submission = await problem.submit(lang, code);
-    
-    // leetcode may take a while to actually compute the results of a submission
-    // periodically checks leetcode to see if submission results are ready
-    for (let i = 0; i < 10; i++) {
-
-        // timeout two seconds
-        await new Promise(r => setTimeout(r, 2000));
-        await submission.detail()
-
-        // if the submission is ready, break the loop
-        if (submission.status !== SubmissionStatus["Submission Not Ready"]) {
-            break;
-        }
-    }
-
-    return submission;
-
-}

d371894478e18f8656172e9259ff4ac5fac543b1
lfu7 <lawrencefu@g.ucla.edu>
2022-03-06 23:37:08 -0800

    added random matchmaking tests

--- test/tracker.ts
+++ test/tracker.ts
@@ -14,6 +14,7 @@ describe("# Tracker Tests", () => {
 
     let tracker = new Tracker();
     const code = tracker.createLobby(TEST_DIFFICULTY);
+
     it("Testing the creation of a Lobby", () => {
         const lobby = tracker.findLobby(code);
         expect(lobby).to.equal(TEST_DIFFICULTY)
@@ -39,6 +40,28 @@ describe("# Tracker Tests", () => {
         tracker.removeLobby(code);
         const lobby =  tracker.findLobby(code);
         expect(lobby).to.be.undefined;
+    });
+
+    it("Testing Random Matchmaking", async () => {
+
+        // first search should not yield a new lobby 
+        const search1 = await tracker.search(TEST_DIFFICULTY);
+        expect(search1[1]).to.equal(false);
+
+        // cancel the search
+        tracker.cancelSearch(search1[0]);
+
+        // second search should not yield a new lobby
+        const search2 = await tracker.search(TEST_DIFFICULTY);
+        expect(search2[1]).to.equal(false);
+
+        // third search should yield a new lobby
+        const search3 = await tracker.search(TEST_DIFFICULTY);
+        expect(search3[1]).to.equal(true);
+
+        const code = search3[0]
+        tracker.removeRace(code);
+        tracker.removeLobby(code);
     });
 
 
